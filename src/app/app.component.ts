73daa4619b208681b2ed798c41dfcf9ad81a599f
Leonard <spazgrafficz@outlook.com>
2018-11-29 16:52:40 +0200

    Changes to the Profile Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { HomePage } from '../pages/home/home';
 import { NearbyOrgPage } from '../pages/nearby-org/nearby-org';
 import { SignUpPage } from '../pages/sign-up/sign-up';
 import { SignInPage } from '../pages/sign-in/sign-in'
+import { ProfilePage } from '../pages/profile/profile';
 @Component({
   templateUrl: 'app.html'
 })
@@ -17,13 +18,8 @@ export class MyApp {
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-<<<<<<< HEAD
-      this.rootPage = NearbyOrgPage;
-      // Okay, so the platform is ready and our plugins are available.
-=======
       this.rootPage = HomePage;
       // Okay, so the plSignInPageatform is ready and our plugins are available.
->>>>>>> 2d7fb56b6fe732cf8d9a5191f19da06b056794d0
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
